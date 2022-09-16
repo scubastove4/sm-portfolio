e8311013c3066237936d7256b223e2a71ca68dc0
Steve Morello <smorello4@gmail.com>
2022-09-16 10:02:20 -0400

    make description a circle. try making both sides more spherical

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,8 +1,8 @@
 const Description = ({ project }) => {
   return (
     <div className="descript-container">
-      <p className="description">{project.description}</p>
       <h2 className="title">{project.title}</h2>
+      <p className="description">{project.description}</p>
       <span className="tech-container">
         <h3 className="tech-title">Technologies Used: </h3>
         <p className="tech">{project.technologies}</p>
