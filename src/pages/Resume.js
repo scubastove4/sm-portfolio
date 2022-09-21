8e9df4e7822ba2c7afc6d157ca3a640a1066fd90
Steve Morello <smorello4@gmail.com>
2022-09-21 15:29:50 -0400

    add react-pdf. test google drive link for file source

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,26 @@
+import { Document } from 'react-pdf/dist/esm/entry.webpack5'
+import resume from '../Resume/SMorelloResume.pdf'
+
 const Resume = () => {
-  return <div>Resume</div>
+  return (
+    <div>
+      {resume && (
+        <Document
+          file={
+            'https://drive.google.com/file/d/1ol0aSZFwPCpkeuk9pOnZuQViKbcqIH4O/view?usp=sharing'
+          }
+          onLoadError={console.error}
+          style={{ width: '50vw', height: '100%' }}
+        ></Document>
+      )}
+      {/* <object
+      data={resume}
+      type="application/pdf"
+      width="100%"
+      height="100%"
+    ></object> */}
+    </div>
+  )
 }
 
 export default Resume
