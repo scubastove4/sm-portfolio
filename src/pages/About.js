822b7174afdddce442adefbc6f5545d19ede542b
Steve Morello <smorello4@gmail.com>
2022-09-20 21:05:06 -0400

    reformat desktop about section. remove gradient bio text

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section id="about">
       <img src={profPic} alt="Steve" />
-      <h2>Bio</h2>
+      <h2 id="bio-title">Bio</h2>
       <p id="bio">
         I am a software engineer who enjoys travel and learning creative methods
         for crafting user-friendly, interactive applications and software. I
@@ -17,12 +17,19 @@ const About = () => {
         healthcare operations, makes me flexible and adaptable to rapidly
         changing circumstances.
       </p>
-      <h2>Technologies</h2>
-      <p id="libraries">Libraries: JavaScript, Python, SQL, CSS, HTML</p>
-      <p id="frameworks">
-        Frameworks: React, Node, Django, Express, MongoDB, Git
-      </p>
-      <p id="other-skills">Other: GitHub, Firebase, Netlify, Heroku, Surge</p>
+      <h2 id="about-tech-title">Technologies</h2>
+      <span id="libraries">
+        <h3>Libraries:</h3>
+        <p>JavaScript, Python, SQL, CSS, HTML</p>
+      </span>
+      <span id="frameworks">
+        <h3>Frameworks:</h3>
+        <p>React, Node, Django, Express, MongoDB, Git</p>
+      </span>
+      <span id="other-skills">
+        <h3>Other:</h3>
+        <p>GitHub, Firebase, Netlify, Heroku, Surge</p>
+      </span>
     </section>
   )
 }
