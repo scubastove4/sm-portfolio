5d1c07746f1c302600a96d414eb49a89f84cd7ae
Steve Morello <smorello4@gmail.com>
2022-09-23 14:42:24 -0400

    redo projects and pd pages for mobile responsiveness with test user info

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -16,23 +16,39 @@ const ProjectDetails = ({ loadProject, projectDetail }) => {
         <div id="proj-det-pg-container">
           <h2 id="pd-title">{projectDetail.title}</h2>
           {/* </span> */}
-          <img
-            src={projectDetail.image}
-            alt={projectDetail.image ? projectDetail.name : 'image pending'}
-          />
-          <span id="pd-tech-container">
-            <h3 id="pd-tech-title">Technologies Used: </h3>
-            <p id="pd-tech">{projectDetail.technologies}</p>
-          </span>
+          <div id="pd-img-tech">
+            <img
+              src={projectDetail.image}
+              alt={projectDetail.image ? projectDetail.name : 'image pending'}
+            />
+            <span id="pd-tech-container">
+              <h3 id="pd-tech-title">Technologies Used: </h3>
+              <p id="pd-tech">{projectDetail.technologies}</p>
+            </span>
+          </div>
           <p id="pd-description">{projectDetail.description}</p>
-          <span id="pd-planned-updates">
-            <h3 id="pd-update-title">Planned Updates: </h3>
-            {/* <p id="pd-updates">{projectDetail.updates}</p> */}
-          </span>
-          <span id="pd-challenges-container">
-            <h3 id="pd-challenges-title">Build Challenges: </h3>
-            {/* <p id="pd-challenges">{projectDetail.challenges}</p> */}
-          </span>
+          <div id="pd-bottom-row">
+            {projectDetail.testUser && (
+              <span id="pd-test-container">
+                <h3 id="pd-test-title">Test User Info</h3>
+                <p id="pd-test-user">{projectDetail.testUser}</p>
+                <p id="pd-test-password">{projectDetail.testPassword}</p>
+                {projectDetail.adminLink && (
+                  <a href={projectDetail.adminLink}>
+                    <button>Admin Link</button>
+                  </a>
+                )}
+              </span>
+            )}
+            <span id="pd-planned-updates">
+              <h3 id="pd-update-title">Planned Updates: </h3>
+              {/* <p id="pd-updates">{projectDetail.updates}</p> */}
+            </span>
+            <span id="pd-challenges-container">
+              <h3 id="pd-challenges-title">Build Challenges: </h3>
+              {/* <p id="pd-challenges">{projectDetail.challenges}</p> */}
+            </span>
+          </div>
         </div>
       ) : null}
     </div>
