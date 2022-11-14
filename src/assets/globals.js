afac2cf4409f888d93ad68828d21f2457cc693e8
Steve Morello <smorello4@gmail.com>
2022-11-14 17:24:05 -0500

    add updates and challenges to global

--- src/assets/globals.js
+++ src/assets/globals.js
@@ -15,7 +15,9 @@ const projects = [
     technologies: 'HTML, CSS, Vanilla JavaScript, and Git/GitHub',
     site: 'https://runthehorses.surge.sh/',
     repo: 'https://github.com/scubastove4/Run-the-Horses',
-    featured: false
+    featured: false,
+    plannedUpdates: [],
+    challenges: ['Dynamic "track" size']
   },
   {
     id: '2',
@@ -29,7 +31,9 @@ const projects = [
       'MongoDB/Mongoose, Express, React, Node.js, Git/GitHub, and Open Brewery DB',
     site: 'https://deja-brew-reviews.herokuapp.com/',
     repo: 'https://github.com/scubastove4/Deja_Brew',
-    featured: true
+    featured: true,
+    plannedUpdates: ['Responsive design', 'Integrate Google Maps API'],
+    challenges: ['First fullstack deployment bugs', 'Integrating Google Maps']
   },
   {
     id: '3',
@@ -38,12 +42,17 @@ const projects = [
     testUser: 'Login: testuser1',
     testPassword: 'Password: 12345678',
     description:
-      '\nTwitter for fitness enthusiasts.\nAs a group, we collaborated on custom frontend and backend REST API.\nI managed user auth, code review/debugging, and Github backend branch policies/version control.',
+      'Twitter for fitness enthusiasts. As a group, we collaborated on custom frontend and backend REST API. I managed user auth, code review/debugging, and Github backend branch policies/version control.',
     technologies:
       'PostgreSQL/Sequelize, Express, React, Node.js, Git/GitHub, BCrypt, JWT, Multer, and Firebase',
     site: 'https://get-fitter-two.herokuapp.com/',
     repo: 'https://github.com/scubastove4/fitter-frontend',
-    featured: true
+    featured: true,
+    plannedUpdates: ['Responsive design', 'More intuitive flow'],
+    challenges: [
+      'First time doing group Git',
+      'Implementing optional file upload'
+    ]
   },
   {
     id: '4',
@@ -54,12 +63,14 @@ const projects = [
     testPassword: 'Passwords: 12345678',
     adminLink: 'https://www.buy-my-stuff-online.com/admin',
     description:
-      '\nEcommerce site, with separate admin and customer models, that conditionally render specific functionality.\nThe goal of this project is to make an intuitive, easy-to-use template that a growing small business can replicate and modify to suit their needs.',
+      'Ecommerce site, with separate admin and customer models, that conditionally render specific functionality. The goal of this project is to make an intuitive, easy-to-use template that a growing small business can replicate and modify to suit their needs.',
     technologies:
       'PostgreSQL/Sequelize, Express, Vue, Node.js, Git/GitHub, BCrypt, JWT, Multer, Firebase, and Stripe',
     site: 'https://www.buy-my-stuff-online.com',
     repo: 'https://github.com/scubastove4/buy-my-stuff',
-    featured: true
+    featured: true,
+    plannedUpdates: ['Responsive for web', 'Adjust credit card entry styling'],
+    challenges: ['Integrating Stripe for payment processing']
   }
 ]
 
